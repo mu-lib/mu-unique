7a88a6a643276e1c31fa5b6c2e7fd09ed2730cb4
Eyal Arubas <eyalarubas@gmail.com>
2014-11-12 11:49:01 +0800

    package changes
    Remove build step. UMD loader is now hard-coded into source files.

--- src/sorter.js
+++ src/sorter.js
@@ -1,9 +1,19 @@
-/**
- * @name sorter
- */
+(function() {
+    'use strict';
 
-function factory() {
-    return function (arr, order) {
-        Array.prototype.sort.call(arr, order);
+    /* istanbul ignore next */
+    if (typeof define === 'function' && define.amd) {
+        define([], factory);
+    } else if (typeof exports === 'object') {
+        module.exports = factory();
+    } else {
+        throw Error("no module loader found");
     }
-}
+
+    function factory() {
+        return function (arr, order) {
+            Array.prototype.sort.call(arr, order);
+        };
+    }
+
+}());
